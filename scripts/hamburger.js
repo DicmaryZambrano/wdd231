c8306b6ceded8bec0b73e0e3b2423ba55ada8f1e
dicmaryzambrano <dicmaryzambrano@outlook.com>
2025-05-11 01:55:44 -0400

    Created wdd231 personal page

---
+++ scripts/hamburger.js
@@ -0,0 +1,4 @@
+document.getElementById("HamMenu").addEventListener("click", function () {
+  document.querySelector("nav").classList.toggle("active");
+  document.querySelector(".menu-toggle").classList.toggle("open");
+});
